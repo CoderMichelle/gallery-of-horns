87b5892ecf20d18e4432722ec53a2785eda60213
mrpresent1@gmail.com <codermichelle@DESKTOP-89FL1O1.localdomain>
2021-03-24 22:07:33 -0700

    bootstrap and clickbutton w heart

--- src/HornedBeasts.js
+++ src/HornedBeasts.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class HornedBeasts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timesClicked: 0
+    };
+  }
+
+  imageClicked = () => {
+    console.log(this.state.timesClicked);
+    this.setState({
+      timesClicked: this.state.timesClicked + 1
+    });
+  }
   render() {
     return (
-      <div>
-        <h2>{this.props.title}</h2>
-        <img src={this.props.image_url} alt={this.props.description} title={this.props.title} />
-        <p>{this.props.description}</p>
-      </div>
+      <Container>
+        <Row>
+          <Col>
+            <h2>{this.props.title}</h2>
+            <button onClick={this.imageClicked} ><img src={this.props.image_url} alt={this.props.description} title={this.props.title} />&#9829;</button>
+            <p>{this.props.description}</p>
+          </Col>
+        </Row>
+      </Container>
     );
 
   }
