a96468a5d7f2a6e61d8fd7cca02b371bb0112358
mrpresent1@gmail.com <codermichelle@DESKTOP-89FL1O1.localdomain>
2021-03-25 22:08:47 -0700

    final lab02

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 import Header from './Header.js';
 import Main from './Main.js';
 import Footer from './Footer.js';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
   render() {
--- src/HornedBeasts.js
+++ src/HornedBeasts.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+// import  from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+
 
 class HornedBeasts extends React.Component {
   constructor(props) {
@@ -10,7 +10,6 @@ class HornedBeasts extends React.Component {
       timesClicked: 0
     };
   }
-
   imageClicked = () => {
     console.log(this.state.timesClicked);
     this.setState({
@@ -19,20 +18,18 @@ class HornedBeasts extends React.Component {
   }
   render() {
     return (
-      <Container fluid >
-        <Row>
-          <Col onClick={this.imageClicked}>
-            <h2>{this.props.title}</h2>
-            <img src={this.props.image_url} alt={this.props.description} title={this.props.title} />
-            <h3>&#9829; {this.state.timesClicked}</h3>
-            <p>{this.props.description}</p>
-          </Col>
-        </Row>
-      </Container>
-    );
 
+      <Card style={{ width: '25rem' }} onClick={this.imageClicked}>
+        <Card.Img variant="top" src={this.props.image_url} style={{ width: '100%' }} alt={this.props.keyword} />
+        <Card.Body>
+          <Card.Title>{this.props.title}</Card.Title>
+          <Card.Text>{this.props.description}</Card.Text>
+          <Card.Text>&#9829; {this.state.timesClicked}</Card.Text>
+        </Card.Body>
+      </Card>
+    );
   }
 }
 export default HornedBeasts;
 
-{/* <button onClick={this.imageClicked} ><img src={this.props.image_url} alt={this.props.description} title={this.props.title} /></button> */ }
+/* <button onClick={this.imageClicked} ><img src={this.props.image_url} alt={this.props.description} title={this.props.title} /></button> */
